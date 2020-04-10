1c20f5128f1d562407eefb998f0ca5677fe5c409
kashyap_kk <A00209673@mycambrian.ca>
2020-04-10 19:10:12 +0000

    added model

--- private/app/models/authorsmodel.php
+++ private/app/models/authorsmodel.php
@@ -6,12 +6,24 @@ class AuthorsModel extends Model {
         parent::__construct();
     }
 
-    function Index () {
-        $this->view("template/header");
-        $this->view("test/test");
-        $this->view("template/footer");
+    function authenticateUser($username,$password){
+        $clean_username = htmlentities($username);
+        $clean_password = htmlentities($password);
+        $sql = "SELECT `hash_password`, `first_name`, `last_name` from authors where email= ?";
+        $stmt = $this->db->prepare($sql);
+        $count = $stmt->execute(Array($clean_username));
+        $row = $stmt->fetch();
+        $hash_password = $row[0];
+        if(isset($hash_password)){
+            $is_authenticated = password_verify($clean_password,$hash_password);
+            if($is_authenticated){
+                $_SESSION["first_name"] = $row[1];
+                $_SESSION["last_name"] = $row[2];
+                $_SESSION_["username"] = $clean_username;
+            }
     } 
 
+}
 }
 
 ?>
