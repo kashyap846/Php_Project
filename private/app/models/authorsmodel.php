887c3b3cb643f4405d30485630ded6ab7d21d38b
kashyap_kk <A00209673@mycambrian.ca>
2020-04-10 18:52:04 +0000

    added model

---
+++ private/app/models/authorsmodel.php
@@ -0,0 +1,17 @@
+<?php
+
+class AuthorsModel extends Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function Index () {
+        $this->view("template/header");
+        $this->view("test/test");
+        $this->view("template/footer");
+    } 
+
+}
+
+?>
