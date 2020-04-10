8ab09632717aca25d1b20f3ad44565dc44a8d86d
kashyap_kk <A00209673@mycambrian.ca>
2020-04-10 21:27:22 +0000

    adding login

--- private/app/views/test/login.php
+++ private/app/views/test/login.php
@@ -1,7 +1,13 @@
-<form action="/" method="POST">
-<input type="hidden" value = "<?PHP echo($CSRF_Token) ?>">
-<label for="username">Username</label>
-<input type="text" id="username" name="username">
+<style>
+form > *{
+    display:block;
+}
+</style>
+
+<form action="/user/login" method="POST">
+<!-- <input type="hidden" value = "<?PHP echo($CSRF_Token) ?>"> -->
+<label for="username">Email</label>
+<input type="text" id="username" name="username" required autocomplete>
 <label for="pass">Password</label>
 <input type="password" id="pass" name="pass">
 <input type="submit" value="submit">
