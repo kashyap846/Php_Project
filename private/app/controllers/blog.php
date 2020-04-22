4fa3fe8029de07afd309f63eedaa9901723f3d57
kashyap_kk <A00209673@mycambrian.ca>
2020-04-22 19:46:45 +0000

    commit

--- private/app/controllers/blog.php
+++ private/app/controllers/blog.php
@@ -7,14 +7,20 @@ class  Blog extends Controller
     function Index(){
         $this->model("BlogModel");
         $posts = $this->BlogModel->getAllPosts();
+        $input = Array("posts" => $posts);
 
         $this->view("template/header");
-        $this->view("blog/index", $posts);
-        echo("hello there");
+        $this->view("blog/index", $input);
         $this->view("template/footer");
 
     }
-    function Read($blogId){
+    function Read($postId){
+        $this->model("BlogModel");
+       $post = $this->BlogModel->getPostById($postId);
+       $this->view("template/header");
+        $this->view("blog/post", $post);
+        
+        $this->view("template/footer");
         
 
     }function Create(){
--- private/app/models/blogmodel.php
+++ private/app/models/blogmodel.php
@@ -12,6 +12,14 @@ class BlogModel extends Model {
         $stmt->execute();
         return $stmt->fetchAll();
 }
+    function getPostById($postId){
+    
+        $sql= "SELECT title, content, author, post_date FROM posts WHERE slug = ?";
+        $stmt= $this->db->prepare($sql);
+        $stmt->execute(Array($postId));
+        return $stmt->fetch();
+}
+
 }
 
 ?>
