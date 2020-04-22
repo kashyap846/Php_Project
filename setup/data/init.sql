ac33c1a5dae0f26143d55d19ec54d28facaa2369
kashyap_kk <A00209673@mycambrian.ca>
2020-04-22 18:05:52 +0000

    commit

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -11,3 +11,18 @@ last_login_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO authors (email, hash_password, first_name, last_name, biography) VALUES ("kashyap846@gmail.com","$2y$10$TI16h5cBne/Z4.zZJLvvcOi.ar9gFkLvY5G0O9Upz36IendzgwzuG","kashyap","KK","Professor");
+
+
+CREATE TABLE posts (
+slug VARCHAR(128) NOT NULL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+content TEXT,
+author VARCHAR(128) NOT NULL,
+
+post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+INDEX (author), 
+FOREIGN KEY (author)
+REFERENCES authors (email)
+);
+INSERT INTO `posts` (slug, title, content, author) 
+VALUES ("post-a", "Post A", "<aritcle><h2>Post A</h2><section><p>This is a basic aritcle</p></section></article>","kashyap846@gmail.com");
