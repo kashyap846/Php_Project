3a9b047d28cb2bec7647daa822d5d6b50a88a835
kashyap_kk <A00209673@mycambrian.ca>
2020-04-22 04:37:00 +0000

    Kashyap

--- private/app/controllers/user.php
+++ private/app/controllers/user.php
@@ -14,10 +14,11 @@ class User extends Controller {
         for($i = 3;$i < $count; $i++){
             $splits[$i-3] = $url_split[$i];
         }
-        echo($_SESSION["username"]);
+        //echo($_SESSION["username"]);
         // echo($count . "<br><br>");
         // print_r($url_split);
-        return join("/",$splits);
+       // echo(join("/",$splits));
+        return join("/",$splits) . "/";
     }
 
     function Index () {
